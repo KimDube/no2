bba234112ef8201c30f7d28b1f92ad5a7266d03a
KimDube <krd020@mail.usask.ca>
2018-06-18 16:06:25 -0600

    Updated SAGE II code to plot monthly mean anomaly at sunset

--- sage.py
+++ sage.py
@@ -1,56 +1,64 @@
-import warnings
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
-from astropy.time import Time
+import pandas as pd
+
 
 from pysagereader.sage_ii_reader import SAGEIILoaderV700
+
+import xarray as xr
 
 
 def sage_ii_no2_time_series(data_folder):
     """
-    Basic example of using the loader to plot the monthly averaged no2 in the tropics
+    plot the monthly averaged no2 in the tropics
     """
 
     # load the data
     sage = SAGEIILoaderV700()
     sage.data_folder = data_folder
-    data = sage.load_data('2000-1-1', '2005-8-31', -20, 20)
+    data = sage.load_data('1984-10-24', '1994-8-31', -10, 10)
 
-    # setup the time bins
-    time_res = 30
-    mjds = np.arange(np.min(data['mjd']), np.max(data['mjd']), time_res)
+    # convert dates
+    dates = pd.to_datetime(data['mjd'], unit='d', origin=pd.Timestamp('1858-11-17'))
 
     # get rid of bad data
     data['NO2'][data['NO2'] == data['FillVal']] = np.nan
-    data['NO2'][data['NO2_Err'] > 10000] = np.nan
+    data['NO2'][data['NO2_Err'] > 1000] = np.nan  # uncertainty greater than 10%
+    data['NO2'][data['Type_Sat'] == 0] = np.nan  # remove sunrise events
 
     # get no2 altitudes
     no2_alts = data['Alt_Grid'][(data['Alt_Grid'] >= data['Range_NO2'][0]) & (data['Alt_Grid'] <= data['Range_NO2'][1])]
 
-    # average the no2 profiles
-    no2 = np.zeros((len(no2_alts), len(mjds)))
-    for idx, mjd in enumerate(mjds):
-        good = (data['mjd'] > mjd) & (data['mjd'] < mjd + time_res)
-        with warnings.catch_warnings():  # there will be all nans at high/low altitudes, its fine
-            warnings.simplefilter("ignore", category=RuntimeWarning)
-            no2[:, idx] = np.nanmean(data['NO2'][good, :], axis=0)
+    # Create dataset
+    ds = xr.Dataset({'NO2': (['time', 'altitude'], data['NO2'])},
+                    coords={'altitude': no2_alts,
+                    'time': dates})
 
-    plot_data(no2_alts, mjds + time_res/2, no2)
+    monthly_no2 = ds.resample(time='MS').mean('time')
+    monthlymeans = monthly_no2.groupby('time.month').mean('time')
+    anomalies = monthly_no2.groupby('time.month') - monthlymeans
+    anomalies = anomalies.groupby('time.month') / monthlymeans
 
-
-def plot_data(alts, mjds, val):
-
-    # make the plot
-    plt.contourf(Time(mjds, format='mjd').datetime, alts, val, levels=np.arange(0, 4e9, 1e8), extend='both')
-    plt.colorbar()
-    #plt.clim(0,7e12)
+    sns.set(context="talk", style="white", rc={'font.family': [u'serif']})
+    fig, ax = plt.subplots(figsize=(10, 5))
+    fax = anomalies.NO2.T.plot.contourf(x='time', y='altitude', robust=True, levels=np.arange(-0.5, 0.5, 0.01),
+                                        extend='both', add_colorbar=0, cmap="RdBu_r")
     plt.ylabel('Altitude [km]')
+    plt.ylim(20, 50)
+    plt.xlabel('')
     locs, labels = plt.xticks()
-    plt.setp(labels, rotation=45)
-    plt.ylim(15, 40)
+    plt.setp(labels, rotation=0)
+    # plt.title('Monthly Mean SAGE II O$\mathregular{_3}$ (-10 to 10 deg. lat.)')
+    plt.title('Monthly Mean SAGE II NO$\mathregular{_2}$ @ Sunset (-10 to 10 deg. lat.)')
+    cb = fig.colorbar(fax, orientation='horizontal', fraction=0.2, aspect=50)
+    # cb.set_label("O$\mathregular{_3}$ Anomaly")
+    cb.set_label("NO$\mathregular{_2}$ Anomaly")
+    plt.tight_layout()
+    plt.savefig("/home/kimberlee/Masters/NO2/Figures/sage_no2_anomaly.png", format='png', dpi=150)
+    plt.show()
 
 
 if __name__ == "__main__":
     sage_ii_no2_time_series('/home/kimberlee/ValhallaData/SAGE/Sage2_v7.00/')
-    plt.show()
